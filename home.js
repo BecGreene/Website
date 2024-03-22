61063256800007b84c03b357e36af2e0a40c1a5c
Rebecca (Bec) Greene <rgreene0003@gmail.com>
2024-03-21 22:16:26 -0400

    - fixing spelling errors - Added new fun fact

--- home.js
+++ home.js
@@ -20,14 +20,15 @@ const projectsButton= document.querySelector("#projects-button");
 // List of fun facts
 funFactArray.push(
     "I am a big Vtuber fan, with my oshis (my favorites) being Gavis Bettel, Banzoin Hakka, Goldbullet, and my kamioshi (my favorite vtuber) being Josuiji Shinri from Holostars EN (POP POP POP!)",
-    "I am a recovering picky eater and I will try anything once. Yes, I will try balut when I have the chance",
-    "I love ice skating and have taken lessons when I was younger, but I still can't spin",
-    "I love the horror genre, but only when it's static (Books and comics). I currently cannot handle horror that moves (Video games and movies)",
-    "I love tattoos, but I am still waiting to get my first one",
-    "I casually collect rubber ducks and they are my children",
-    "My favorite game series of all time is Yakuza / Like a Dragon / Ryu Ga Gotoku, and my favorite game of all time is Yakuza: Like a Dragon (<= HIGHLY RECCOMEND)",
+    "I am a recovering picky eater and I will try anything once. Yes, I will try balut when I have the chance.",
+    "I love ice skating and have taken lessons when I was younger, but I still can not spin.",
+    "I love the horror genre, but only when it is static (Books and comics). I currently cannot handle horror that moves (Video games and movies)",
+    "I love tattoos, but I am still waiting to get my first one.",
+    "I casually collect rubber ducks and they are my children.",
+    "I am a true plushie lover until the sun fizzles out.",
+    "My favorite video game series of all time is Yakuza / Like a Dragon / Ryu Ga Gotoku, and my favorite video game of all time is Yakuza: Like a Dragon (<= HIGHLY RECCOMEND)",
     "I love visual novel puzzle games such as Ace Attorney, Professor Layton, and Ghost Trick: Phantom Dective (<= HIGHLY RECCOMEND)",
-    "My favorite animes are Jojo's Bizarre Adventure, Sk8 the Infinity, Lupin the Third, and Golden Kamuy (<= HIGHLY RECCOMEND)",
+    "My favorite animes are Jojo's Bizarre Adventure, Sk8 the Infinity, Lupin the Third, Castlevania, and Golden Kamuy (<= HIGHLY RECCOMEND)",
     "I am a big Webtoon reader, with my favorites being Not So Shoujo Love Story, Castle Swimmer, Daybreak, Cursed Princess Club, The Doctors are Out, Mage & Demon Queen, Stagtown, TACIT, Bastard, Lavender Jack, and Gourmet Hound (<= HIGHLY RECCOMEND)"
 );
 
